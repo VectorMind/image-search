36a271a2ec731924ab2e5bcba7383ddd164ee79f
wass <wassim.filali@gmail.com>
2024-08-04 11:02:00 +0200

    refactor generation

--- images_features.py
+++ images_features.py
@@ -3,27 +3,31 @@ from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import utils as utl
 
-def image_to_embedding(image_path):
-    # Load the pre-trained CLIP model
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    image = Image.open(image_path)
-    inputs = processor(images=image, return_tensors="pt", padding=True)
-    outputs = model.get_image_features(**inputs)
+def image_embedding_clip(image,model):
+    inputs  = model["processor"](images=image, return_tensors="pt", padding=True)
+    outputs = model["model"].get_image_features(**inputs)
     embedding_vector = outputs.detach().numpy()
-
     return embedding_vector
 
-def process_images_in_folder(folder_path):
+def generate_images_embedding(folder_path,model_name):
     images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if (f.endswith('.png') or f.endswith('.jpg'))]
+    for image_path in images:
+        image_path = image_path.replace("\\","/")
     utl.save_json(images,"data/images.json")
     # Process each image and store/print embeddings
     embeddings = {}
     for image_path in images:
-        embedding = image_to_embedding(image_path)
+        image = Image.open(image_path)
+        embedding = image_embedding_clip(image,models[model_name])
         print(f"Generated Embedding Vector for {image_path}:")
         embeddings[image_path] = embedding.tolist()
-    utl.save_json(embeddings,"data/embeddings.json")
+    utl.save_json(embeddings,f"data/embeddings-{model_name}.json")
+
+clip = {
+    "model": CLIPModel.from_pretrained("openai/clip-vit-base-patch32"),
+    "processor": CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+}
+models = {"clip":clip}
 
 if __name__ == "__main__":
-    process_images_in_folder("images")
+    generate_images_embedding("images","clip")
